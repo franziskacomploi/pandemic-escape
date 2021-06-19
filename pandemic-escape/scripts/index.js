29c010d718f169ebe4253eefb28ef09c4229e262
franziska pendzialek <franziska.pendzialek@outlook.com>
2021-06-19 17:35:44 +0200

    add level 1 info screen, add level 1 canvas

--- pandemic-escape/scripts/index.js
+++ pandemic-escape/scripts/index.js
@@ -3,6 +3,12 @@
 let startScreen = document.getElementById('home');
 let startButton = document.querySelector(".startButton");
 
+let infoOneScreen = document.getElementById('infoOne');
+
 startButton.addEventListener('click', () => {
 startScreen.classList.add('hidden');
+infoOneScreen.classList.remove('hidden');
 })
+
+
+
