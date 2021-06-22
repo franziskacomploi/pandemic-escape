002cd42e28dc5fcb1f2cdb2a405714248f050959
franziska pendzialek <franziska.pendzialek@outlook.com>
2021-06-22 19:10:50 +0200

    add game logic comments

--- pandemic-escape/scripts/index.js
+++ pandemic-escape/scripts/index.js
@@ -10,6 +10,8 @@ document.getElementById('infoOne').classList.remove('hidden');
 document.querySelector(".infoOneButton").addEventListener('click', () => {
     document.getElementById('infoOne').classList.add('hidden');
     document.getElementById('levelOne').classList.remove('hidden');
+    // eventually Countdown to startGame
+    startLevelOne();
 
 });
 
@@ -18,8 +20,30 @@ document.querySelector(".infoOneButton").addEventListener('click', () => {
 const canvasOne = document.getElementById("levelOneCanvas");
 let ctxOne = canvasOne.getContext("2d");
 
+// Task 1
+// Timer that runs out = 60 seconds --> SetInterval mit 1000 ms
+// "Lives" --> DOM
+// Counter that counts points --> DOM
+// Start Level 1 Button
 
-// Add Class Hidden to Level One to get to Info Screen Two - You loos or win?
+// Task 2
+// Canvas
+// Background that moves --> image looping
+// Class Player needed: Mouth 
+// Control via Arrows
+// Class Counterpart = Masks, Virus
+// "Obstacles" = Virus --> How do they move?
+// "Points" = Masks
+
+// Task 3
+// Start Game Function Game Logic (collect points etc.)
+
+function startLevelOne() {
+
+}
+
+
+// Add Class Hidden to Level One to get to Info Screen Two - You loose or next level? = Section mit class hidden
 
 // Info Screen Two
 
@@ -32,3 +56,22 @@ document.querySelector(".infoTwoButton").addEventListener('click', () => {
 
 const canvasTwo = document.getElementById("levelTwoCanvas");
 let ctxTwo = canvasTwo.getContext("2d");
+
+// Timer that runs out --> Same as Level 1
+// Counter that counts points --> Same as Level 1
+// "Lives" --> Same as Level 1
+
+// Canvas
+// Background --> Same as Level 1 (maybe different image)
+// Class Player = Mouth --> Same as Level 1
+// Control via Arrows --> Same as Level 1
+// Class Counterpart: Virus, Masks, Needles, "Querdenker" --> Querdenker, Needles as an addition
+// Masks = more points --> Same as Level 1
+// Virus, Querdenker (quicker than virus) = costs a live --> Same as Level 1
+// Needles = You need at least two to win --> New as an addition
+
+
+// You loose = Section mit class hidden
+
+// You won! = Section mit class hidden - add "You collected XX masks!"
+// Highscore for Mask Points
